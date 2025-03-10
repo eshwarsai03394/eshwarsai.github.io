14cfebc27923c7ba107d3847a5ca3a2234c1e8fc
eswar.s <eswar.s@caratlane.com>
2025-03-10 14:47:19 +0530

    deploy commit

--- my-portfolio/src/router/index.js
+++ my-portfolio/src/router/index.js
@@ -1,25 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../views/Home.vue';
-// import About from '../views/About.vue';
-// import Skills from '../views/Skills.vue';
-// import Blogs from '../views/Blogs.vue';
-// import Experience from '../views/Experience.vue';
-// import Contact from '../views/Contact.vue';
-
 
 const routes = [
     { path: '/', component: Home },
-    // { path: '/about', component: About },
-    // { path: '/skills', component: Skills },
-    // { path: '/blogs', component: Blogs },
-    // { path: '/experience', component: Experience },
-    // { path: '/contact', component: Contact },
-  ];
-  
+    // Your commented routes
+];
 
-  const router = createRouter({
-    history: createWebHistory(),
+const router = createRouter({
+    history: createWebHashHistory(),
     routes,
-  });
-  
-  export default router;  
+});
+
+export default router;
