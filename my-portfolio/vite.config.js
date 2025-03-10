33d33b3d17edb96c44013943aac4924204473aab
eswar.s <eswar.s@caratlane.com>
2025-03-10 11:10:08 +0530

    vite changes

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -3,11 +3,22 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/eshwarsai03394.github.io/eshwarsai.github.io/',
+  base: "/",
   plugins: [vue()],
   resolve: {
     alias: {
       "@": "/src/main.js",
+    }
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].[hash].js`,
+        chunkFileNames: `[name].[hash].js`,
+        assetFileNames: `[name].[hash].[ext]`,
+        dir: 'dist' // Ensures all files are placed in 'dist/' directly
+      }
     }
   }
 })
